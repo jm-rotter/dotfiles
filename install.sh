e8b0514f93efba245b796f018329a11c2539b232
Jaden Rotter <jaden.rotter@tum.de>
2025-03-09 16:55:29 +0100

    installer

--- install.sh
+++ install.sh
@@ -33,6 +33,10 @@ curl -sL zplug.sh/installer | zsh
 echo "Moving zshrc"
 mv -f ~/dotfiles/.zshrc ~/
 mv ~/dotfiles/aliases.zsh ~/.oh-my-zsh/custom/
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
 source ~/.zshrc
 
 # Optional: Set
