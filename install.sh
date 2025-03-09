7d5325d074045be058ee1bae5d729f550482656d
Jaden Rotter <jaden.rotter@tum.de>
2025-03-09 16:42:30 +0100

    installer

--- install.sh
+++ install.sh
@@ -18,23 +18,23 @@ sudo apt install -y \
 	ranger \
 
 
-mv ~\dotfiles\.zshrc ~\
 
 
 echo "Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 curl -sL zplug.sh/installer | zsh
-echo "source ~/.zplug/init.zsh" >> ~/.zshrc
-echo 'zplug "zsh-users/zsh-syntax-highlighting"' >> ~/.zshrc
-echo 'zplug "zsh-users/zsh-autosuggestions"' >> ~/.zshrc
+#echo "source ~/.zplug/init.zsh" >> ~/.zshrc
+#echo 'zplug "zsh-users/zsh-syntax-highlighting"' >> ~/.zshrc
+#echo 'zplug "zsh-users/zsh-autosuggestions"' >> ~/.zshrc
 
-echo 'export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)' >> ~/.zshrc
-echo 'export ZSH_AUTOSUGGEST_STRATEGY=(completion)' >> ~/.zshrc
+#echo 'export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)' >> ~/.zshrc
+#echo 'export ZSH_AUTOSUGGEST_STRATEGY=(completion)' >> ~/.zshrc
+
+mv -f ~/dotfiles/.zshrc ~/
+mv ~/dotfiles/aliases.zsh ~/.oh-my-zsh/custom/
 source ~/.zshrc
 
-zplug install
-#
 # Optional: Set
 # Zsh as the default shell
 chsh -s $(which zsh)
