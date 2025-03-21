8b64a2878019e02200d9a9ae50dd1b1ae0bd8efb
Jaden Rotter <jaden.rotter@tum.de>
2025-03-21 09:30:49 -0700

    added node and rust

--- install.sh
+++ install.sh
@@ -19,6 +19,19 @@ sudo apt install -y \
 	xcape \
     fzf \
 	unzip \
+	pkg-config\
+
+echo "Installing Rust and Cargo"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+cargo install tree-sitter-cli
+
+echo "Installing node & npm"
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install npm
+
+
 
 echo "Installing Oh My Zsh..."
 RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
